ae21be591b8a639372d65d90d885448002b32c5e
Rohit <r4rohit002@gmail.com>
2021-06-20 12:15:14 -0700

    Added click listener in WordListAdapter and added Implicit intent

--- app/src/main/java/com/rohitksingh/kotlinwordapp/DetailActivity.kt
+++ app/src/main/java/com/rohitksingh/kotlinwordapp/DetailActivity.kt
@@ -1,18 +1,19 @@
 package com.rohitksingh.kotlinwordapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.rohitksingh.kotlinwordapp.databinding.ActivityDetailBinding
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(), ItemclickListener {
 
     private lateinit var binding: ActivityDetailBinding
     private val layoutManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
-    private lateinit var adapter : WordListAdapter
+    private lateinit var adapter: WordListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,16 +21,32 @@ class DetailActivity : AppCompatActivity() {
         receiveValue()?.let { initRecyclerView(it) }
     }
 
-    private fun initBinding(){
+    override fun itemClicked(word: String) {
+        searchWordInBrowser(Uri.parse(SEARCH_PREFIX + word))
+    }
+
+    private fun initBinding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
     }
 
-    private fun initRecyclerView(startingLetter: String){
+    private fun initRecyclerView(startingLetter: String) {
         adapter = WordListAdapter(this, startingLetter[0])
         binding.wordRecyclerView.layoutManager = layoutManager
         binding.wordRecyclerView.adapter = adapter
     }
 
     private fun receiveValue() = intent?.extras?.getString(MainActivity.LETTER_KEY)
+
+    private fun searchWordInBrowser(url: Uri) {
+        val intent = Intent(Intent.ACTION_VIEW, url)
+        startActivity(intent)
+    }
+
+    companion object {
+
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+
+    }
+
 
 }
--- app/src/main/java/com/rohitksingh/kotlinwordapp/WordListAdapter.kt
+++ app/src/main/java/com/rohitksingh/kotlinwordapp/WordListAdapter.kt
@@ -3,6 +3,7 @@ package com.rohitksingh.kotlinwordapp
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.rohitksingh.kotlinwordapp.databinding.ItemWordLetterBinding
 
@@ -10,11 +11,13 @@ class WordListAdapter(context: Context, private val startingLetter: Char) : Recy
 
     private val allWords: List<String> = context.resources.getStringArray(R.array.words).toList()
     private var filteredWords = allWords.filter { it.startsWith(startingLetter, ignoreCase = true) }
+    private val itemClickListener = context as ItemclickListener
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
 
-        val binding = ItemWordLetterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return WordViewHolder(binding)
+        val binding: ItemWordLetterBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context),
+                R.layout.item_word_letter, parent, false)
+        return WordViewHolder(binding, itemClickListener)
     }
 
     override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
@@ -23,10 +26,12 @@ class WordListAdapter(context: Context, private val startingLetter: Char) : Recy
 
     override fun getItemCount() = filteredWords.size
 
-    class WordViewHolder(private val binding: ItemWordLetterBinding) : RecyclerView.ViewHolder(binding.root) {
+    class WordViewHolder(private var binding: ItemWordLetterBinding,
+                         private var itemClickListener: ItemclickListener) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(word: String) {
-            binding.word.text = word
+            binding.setWord(word)
+            binding.itemclicklistener = itemClickListener
         }
 
     }
